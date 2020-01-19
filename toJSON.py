ceade4af8da650ba984fdd3126b5372f77089603
Damien Gaignon <damien.gaignon@gmail.com>
2020-01-19 14:45:29 +0100

    Reduce iteration
    Only eve region is needed.
    Thus, check of regionID is not needed aswell.

--- toJSON.py
+++ toJSON.py
@@ -48,31 +48,29 @@ def importYaml():
     print("Importing Universe Data")
 
     if os.path.exists('invNames.json'):
-        print('Load JSON')
+        print('Load invNames JSON')
         with open('invNames.json') as infile:
             inv_names = json.load(infile)
     else:
-        print('Load YAML')
+        print('Load invNames YAML')
         with open(r'sde/bsd/invNames.yaml') as infile:
             inv_names = yaml.load(infile, Loader = Loader)
 
         with open('invNames.json', 'w') as outfile:
             json.dump(inv_names, outfile)
 
-    print("Importing region static")
-    regions = glob.glob(os.path.join('sde', 'fsd', 'universe', '*', '*', 'region.staticdata'))
-    for region_file in regions:
+    print("Importing region static data")
+    region_files = glob.glob(os.path.join('sde', 'fsd', 'universe', 'eve', '*', 'region.staticdata'))
+    for region_file in region_files:
         region = {}
         head, tail = os.path.split(region_file)
         with open(region_file,'r') as region_yaml:
             regiony = yaml.load(region_yaml, Loader = Loader)
-        if int(regiony['regionID']) > 11000000 and int(regiony['regionID']) < 14000000:
-            continue
-        print("Importing Region {}".format(head))
         for item in inv_names:
             if item['itemID'] == regiony['regionID']:
                 region_name = item['itemName']
                 break
+        print("Importing Region {}".format(region_name))
         region['regionID'] = regiony['regionID']
         region['regionName'] = region_name
         region['x'] = regiony['center'][0]
