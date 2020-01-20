261e05d3f23bceb8239cf96df96a553ed385d897
Damien Gaignon <damien.gaignon@gmail.com>
2020-01-20 10:27:05 +0100

    Change wording

--- sde_to_json.sh
+++ sde_to_json.sh
@@ -10,7 +10,7 @@ then
     exit
 fi
 echo 'Latest commit was :' $(git log -1 --pretty=%B)
-echo 'Building resources files for new revision' $currentrev
+echo 'Building resources files for revision' $currentrev
 git config --global user.email "$GH_USER_EMAIL"
 git config --global user.name "$GH_USER_NAME"
 git remote add origin-ssh git@github.com:$GH_REPO
@@ -21,7 +21,7 @@ git add resources/staStations.json
 git add resources/mapRegionJumps.json
 git add resources/mapConstellationJumps.json
 git add resources/mapSolarSystemJumps.json
-git commit -m "Update DB to $currentrev"
+git commit -m "Up to date DB with resources $currentrev"
 git checkout -b temp
 git checkout -B $TRAVIS_BRANCH temp
 git push --quiet --set-upstream origin-ssh $TRAVIS_BRANCH
