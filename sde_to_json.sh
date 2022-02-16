66b747eee4609926c74eb33d8164e5b6a21d3442
Alexander Whipp <alexruns4@gmail.com>
2022-02-15 22:43:29 -0500

    Swapping Actions in for TravisCI

--- sde_to_json.sh
+++ sde_to_json.sh
@@ -14,7 +14,6 @@ echo 'Latest commit was :' $(git log -1 --pretty=%B)
 echo 'Building resources files for revision' $currentrev
 git config --global user.email "$GH_USER_EMAIL"
 git config --global user.name "$GH_USER_NAME"
-git remote add origin-ssh git@github.com:$GH_REPO
 mkdir -p resources
 python3 toJSON.py
 git add resources/mapRegions.json
@@ -28,6 +27,4 @@ git add resources/stationList.json
 git add resources/stationIdToName.json
 git add resources/regionList.json
 git commit -m "Up to date DB with resources $currentrev"
-git checkout -b temp
-git checkout -B $TRAVIS_BRANCH temp
-git push --quiet --set-upstream origin-ssh $TRAVIS_BRANCH
+git push
