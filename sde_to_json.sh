96c0c3ae804c68129b291bbefbc4b56d941be488
Alexander Whipp <alexruns4@gmail.com>
2022-09-12 16:07:18 -0400

    20220628

--- sde_to_json.sh
+++ sde_to_json.sh
@@ -10,13 +10,16 @@ then
     echo "Already lastest resources"
     exit
 fi
+
 echo 'Latest commit was :' $(git log -1 --pretty=%B)
 echo 'Building resources files for revision' $currentrev
+
 git config --global user.email "$GH_USER_EMAIL"
 git config --global user.name "$GH_USER_NAME"
+
 mkdir -p resources
 python3 toJSON.py
-ls
 git add resources/*
+
 git commit -m "Up to date DB with resources $currentrev"
 git push
