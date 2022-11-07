d5a3dba5558c6a8fc8df2edeb55e7732f09f0d5b
Alexander Whipp <alexruns4@gmail.com>
2022-11-06 22:36:28 -0500

    Remove check. Always update regardless.

--- sde_to_json.sh
+++ sde_to_json.sh
@@ -4,12 +4,6 @@ wget -O page https://developers.eveonline.com/resource/resources
 
 link=$(grep "https.*sde.*zip" page | awk -F '"' '{print $2}')
 currentrev=$(curl -s -v -X HEAD $link 2>&1 | grep '< Last-Modified:' | awk -F ':' '{print $2":"$3":"$4}' | xargs -0 date +"%Y%m%d" -d)
-echo 'Current resources revision :' $currentrev
-if [[ $(git log -1 --pretty=%B) == *"$currentrev"* ]]
-then
-    echo "Already lastest resources"
-    exit
-fi
 
 echo 'Latest commit was :' $(git log -1 --pretty=%B)
 echo 'Building resources files for revision' $currentrev
